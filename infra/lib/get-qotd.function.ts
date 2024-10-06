b1b194b2ecdcd23fb7fd244420c8cef256da2e99
Finlay Hudson <11finnh@gmail.com>
2024-10-06 19:45:27 +0100

    Allow cors

--- infra/lib/get-qotd.function.ts
+++ infra/lib/get-qotd.function.ts
@@ -5,6 +5,11 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
     console.log(`Context: ${JSON.stringify(context, null, 2)}`);
     return {
         statusCode: 200,
+        headers: {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        },
         body: JSON.stringify({
             message: 'not hello world',
         }),
