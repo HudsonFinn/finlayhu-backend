b59c2a1e30a6635d7ac567338d7466397741e706
Finlay Hudson <11finnh@gmail.com>
2024-10-06 22:13:31 +0100

    Add call to get qotd from api

--- infra/lib/get-qotd.function.ts
+++ infra/lib/get-qotd.function.ts
@@ -1,8 +1,46 @@
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from 'aws-lambda';
 
-export const handler = async (event: APIGatewayEvent, context: Context): Promise<APIGatewayProxyResult> => {
-    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-    console.log(`Context: ${JSON.stringify(context, null, 2)}`);
+type Quote = {
+	q?: string;
+	a?: string;
+	i?: string;
+	c?: number;
+};
+
+type Response = Array<Quote> | undefined;
+
+const getQuote = async (): Promise<Quote> => {
+    const quoteOfTheDayUrl = "https://zenquotes.io/api/today";
+    const response = await fetch(quoteOfTheDayUrl);
+    const json = (await response.json()) as Response;
+
+    if (!json || !Array.isArray(json) || json.length < 1) throw new Error("No data or empty array returned");
+    const data = json[0];
+    if (!data.q || !data.a) throw new Error("Quote is missing quote or author");
+    
+    return data;
+}
+
+export const handler = async (_event: APIGatewayEvent, _context: Context): Promise<APIGatewayProxyResult> => {
+    let todaysData;
+    try {
+        todaysData = await getQuote();
+    } catch (e) {
+        return {
+            statusCode: 500,
+            headers: {
+                "Access-Control-Allow-Headers" : "Content-Type",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+            },
+            body: JSON.stringify({
+                message: `Failed to fetch data from https://zenquotes.io/api/today: ${e}`
+            }
+                
+            ),
+        }; 
+    }
+
     return {
         statusCode: 200,
         headers: {
@@ -10,8 +48,6 @@ export const handler = async (event: APIGatewayEvent, context: Context): Promise
             "Access-Control-Allow-Origin": "*",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         },
-        body: JSON.stringify({
-            message: 'not hello world',
-        }),
+        body: JSON.stringify(todaysData),
     };
 };
