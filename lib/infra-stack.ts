29a2e1dfd512d6b9bdbdebead4ae9a4b9723a244
HudsonFinn <11finnh@gmail.com>
2025-09-22 00:18:08 +0100

    Fix cloudformation stack to deploy cloudfront, APIGateway and Lambda

--- lib/infra-stack.ts
+++ lib/infra-stack.ts
@@ -1,10 +1,72 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-import { HelloWorld } from './get-qotd';
-  
+import { Fn, RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { QuoteOfTheDayAPI } from "./get-qotd";
+import {
+  BlockPublicAccess,
+  Bucket,
+  BucketAccessControl,
+  CorsRule,
+  HttpMethods,
+} from "aws-cdk-lib/aws-s3";
+import {
+  CloudFrontAllowedMethods,
+  CloudFrontWebDistribution,
+  Distribution,
+  OriginAccessIdentity,
+  OriginProtocolPolicy,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
+import { HttpOrigin, S3BucketOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
+import path = require("path");
+
 export class InfraStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-    new HelloWorld(this, 'hello-world');
+
+    const s3Bucket = new Bucket(this, "ImportBucket", {
+      removalPolicy: RemovalPolicy.RETAIN,
+    });
+
+    const lambdaFunction = new NodejsFunction(this, "get-qotd", {
+      entry: "./lib/get-qotd.function.ts",
+    });
+
+    const quoteOfTheDayAPI = new LambdaRestApi(this, "get-qotd-api", {
+      handler: lambdaFunction,
+    });
+
+    const s3AOI = new OriginAccessIdentity(this, "s3AOI");
+    s3Bucket.grantRead(s3AOI);
+
+    const backendCloudfront = new Distribution(this, "PersonalSiteCloudfront", {
+      defaultBehavior: {
+        origin: S3BucketOrigin.withOriginAccessIdentity(s3Bucket, {
+          originAccessIdentity: s3AOI,
+        }),
+      },
+      defaultRootObject: "index.html",
+      errorResponses: [
+        {
+          httpStatus: 404,
+          responseHttpStatus: 404,
+          responsePagePath: "/index.html",
+        },
+        {
+          httpStatus: 403,
+          responseHttpStatus: 403,
+          responsePagePath: "/index.html",
+        },
+      ],
+      additionalBehaviors: {
+        "/api/qotd": {
+          origin: new HttpOrigin(
+            `${quoteOfTheDayAPI.restApiId}.execute-api.${this.region}.${this.urlSuffix}`,
+            { originPath: `/${quoteOfTheDayAPI.deploymentStage.stageName}` }
+          ),
+        },
+      },
+    });
   }
 }
